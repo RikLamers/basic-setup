f92a30cf9dcc723091dfc9d8a99d9fdaa6ddd497
Rik Lamers <info@riklamers.com>
2019-10-20 00:26:43 +0200

    imported header js

--- src/components/modules/index.js
+++ src/components/modules/index.js
@@ -1,5 +1,6 @@
 // export { default as Content } from './m-content/js/m-content';
 
+export { default as Header } from './m-header/js/m-header.javascript';
 export { default as Navigation } from './m-navigation/js/m-navigation.javascript';
 export { default as Slider } from './m-slider/js/m-slider.javascript';
 export { default as Tab } from './m-tab/js/m-tab.javascript';
