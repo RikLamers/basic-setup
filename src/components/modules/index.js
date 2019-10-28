f4a146a1d56c69d691b0cbb815c2404350636787
Rik Lamers <info@riklamers.com>
2019-10-28 16:44:54 +0100

    Added text-slider module

--- src/components/modules/index.js
+++ src/components/modules/index.js
@@ -4,3 +4,4 @@ export { default as Header } from './m-header/js/m-header.javascript';
 export { default as Navigation } from './m-navigation/js/m-navigation.javascript';
 export { default as Slider } from './m-slider/js/m-slider.javascript';
 export { default as Tab } from './m-tab/js/m-tab.javascript';
+export { default as TextSlider } from './m-text-slider/js/m-text-slider';
